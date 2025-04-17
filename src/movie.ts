7553023c46468310cd5897403f6ca7d817e8fa21
snakajima <satoshi.nakajima@gmail.com>
2025-04-17 09:06:55 -1000

    format

--- src/movie.ts
+++ src/movie.ts
@@ -140,8 +140,14 @@ const createVideo = (
     } else {
       filterComplexParts.push(
         // Resize background image to match canvas dimensions
-        `[${element.imageIndex}:v]scale=${canvasInfo.width}:${canvasInfo.height},setsar=1,trim=duration=${element.duration}[bg${index}];` +
-          `[${imageCount + index}:v]scale=${canvasInfo.width * 2}:${canvasInfo.height * 2},setsar=1,format=rgba,zoompan=z=zoom+0.0004:x=iw/2-(iw/zoom/2):y=ih-(ih/zoom):s=${canvasInfo.width}x${canvasInfo.height}:fps=30:d=${element.duration * 30},trim=duration=${element.duration}[cap${index}];` +
+        `[${element.imageIndex}:v]scale=${canvasInfo.width}:${canvasInfo.height},` +
+          `setsar=1,` +
+          `trim=duration=${element.duration}[bg${index}];` +
+          `[${imageCount + index}:v]scale=${canvasInfo.width * 2}:${canvasInfo.height * 2},` +
+          `setsar=1,` +
+          `format=rgba,` +
+          `zoompan=z=zoom+0.0004:x=iw/2-(iw/zoom/2):y=ih-(ih/zoom):s=${canvasInfo.width}x${canvasInfo.height}:fps=30:d=${element.duration * 30},` +
+          `trim=duration=${element.duration}[cap${index}];` +
           `[bg${index}][cap${index}]overlay=(W-w)/2:(H-h)/2:format=auto[v${index}]`,
       );
     }
